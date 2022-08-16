e256d4890ed3a05270c8f86e99fe12c6006cd183
sungsuhan <swhanssu@gmail.com>
2022-08-16 18:31:53 +0900

    reactivity

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,16 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/databinding/string">String</router-link> |
-    <router-link to="/databinding/html">Html</router-link> |
-    <router-link to="/databinding/input">Input</router-link> |
-    <router-link to="/databinding/select">Select</router-link> |
-    <router-link to="/databinding/checkbox">CheckBox</router-link> |
-    <router-link to="/databinding/radio">Radio</router-link> |
-    <router-link to="/databinding/attribute">Attribute</router-link> |
-    <router-link to="/databinding/list">List</router-link> |
-    <router-link to="/databinding/class">Class</router-link> |
-    <router-link to="/databinding/style">Style</router-link> |
-    <router-link to="/event/click">Counter</router-link> |
-    <router-link to="/event/change">Change</router-link> |
-    <router-link to="/event/key">Key</router-link>
-  </nav>
-  <router-view/>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
 
 <style>
 #app {
@@ -26,18 +19,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+  margin-top: 60px;
 }
 </style>
